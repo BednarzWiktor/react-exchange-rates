9a27e2c8f7e261e4f220d57cec3259656f4d5d26
Wiktor Bednarz <b.w.roboczy@gmail.com>
2020-07-25 01:23:23 +0200

    Updates

---
+++ precache-manifest.b3eadbcd7dca9535a901d0d78aaf061c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f67be280c18e7792e635bf5e56c48db",
+    "url": "/react-exchange-rates/index.html"
+  },
+  {
+    "revision": "6261a4de9bdb82bf440c",
+    "url": "/react-exchange-rates/static/css/2.6fd840f6.chunk.css"
+  },
+  {
+    "revision": "6b8e1806cf07acac36f9",
+    "url": "/react-exchange-rates/static/css/main.42f27cdf.chunk.css"
+  },
+  {
+    "revision": "6261a4de9bdb82bf440c",
+    "url": "/react-exchange-rates/static/js/2.0ffcbc5a.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/react-exchange-rates/static/js/2.0ffcbc5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8e1806cf07acac36f9",
+    "url": "/react-exchange-rates/static/js/main.8f918be9.chunk.js"
+  },
+  {
+    "revision": "081ed24f6c5ea3b33280",
+    "url": "/react-exchange-rates/static/js/runtime-main.eca667d2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e62bc5c344a1527a970cd9b601152e7f.js"
+  "/react-exchange-rates/precache-manifest.b3eadbcd7dca9535a901d0d78aaf061c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-exchange-rates/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
