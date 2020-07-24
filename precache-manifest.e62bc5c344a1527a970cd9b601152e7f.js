c1103b11e0e3eb438646e8a35a6818f4e6899580
Wiktor Bednarz <b.w.roboczy@gmail.com>
2020-07-25 01:19:53 +0200

    Updates

---
+++ precache-manifest.e62bc5c344a1527a970cd9b601152e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5e61ed490c32f3f08103e2f43ab80c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a48e7868534b04db562",
+    "url": "/static/css/2.6fd840f6.chunk.css"
+  },
+  {
+    "revision": "833f18c70a385b359c69",
+    "url": "/static/css/main.42f27cdf.chunk.css"
+  },
+  {
+    "revision": "0a48e7868534b04db562",
+    "url": "/static/js/2.a7b8c82e.chunk.js"
+  },
+  {
+    "revision": "c9dd85361cb9cef9841c7e9bd1147de5",
+    "url": "/static/js/2.a7b8c82e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833f18c70a385b359c69",
+    "url": "/static/js/main.cfdc0959.chunk.js"
+  },
+  {
+    "revision": "bd9c9baa3a8c008b7444",
+    "url": "/static/js/runtime-main.83de81f5.js"
+  }
+]);
